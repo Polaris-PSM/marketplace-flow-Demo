32ba921c1b922e44378ec979b36f385eda8e7f4f
Eli Greenberg <eli_g@polaris-sys.co.il>
2024-12-25 22:12:33 +0200

    6eh run
    Fix Vite build configuration

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html'
+    }
+  }
 })
